58d08f522a2a4e30a43260632ad867963bfa7db9
Ciprian <ciprian-git@outlook.com>
2016-09-03 12:31:15 +0300

    create templates

--- src/AppBundle/Controller/SymfonyControlerController.php
+++ src/AppBundle/Controller/SymfonyControlerController.php
@@ -6,13 +6,44 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Entity\Symfony;
 
-class SymfonyControlerController extends Controller
-{
+class SymfonyControlerController extends Controller {
     /**
      * @Route("/", name="fav_list")
      */
-    public function indexAction()
-    {
+    public function indexAction() {
         return $this->render('fav/index.html.twig', array());
     }
+
+    /**
+     * @Route("/fav/add", name="add_fav")
+     */
+    public function addAction() {
+        $this->addFlash('notice', 'Favorite added.');
+        return $this->render('fav/add.html.twig', array());
+    }
+
+    /**
+     * @Route("/fav/details/{id}", name="view_fav")
+     */
+    public function viewAction($id) {
+        $this->addFlash('notice', 'Details of favorite: ' . $id . " .");
+        return $this->render('fav/view.html.twig', array());
+    }
+
+    /**
+     * @Route("/fav/edit/{id}", name="edit_fav")
+     */
+    public function editAction($id) {
+        $this->addFlash('notice', 'Edited favorite: ' . $id . " .");
+        return $this->render('fav/edit.html.twig', array());
+    }
+
+    /**
+     * @Route("/fav/delete/{id}", name="delete_fav")
+     */
+    public function deteleAction($id) {
+        $this->addFlash('error', 'Favorite ' . $id . " couldn't be removed.");
+        return $this->redirectToRoute('fav_list');
+    }
+
 }
